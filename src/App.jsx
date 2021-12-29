b0a11228f65d7ee90aa64cf1dc80b0604459fa26
kevinNdahayo <ndahayokevin1@gmail.com>
2021-12-28 17:31:46 -0800

    Added contacts list via props

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,24 @@ import AddContact from './Components/AddContact'
 import ContactList from './Components/ContactList'
 import ContactCard from './Components/ContactCard'
 function App() {
+  const contacts = [
+    {
+      id: 1,
+      name: 'KevinNdahayo',
+      email: 'Ndahayokevin1@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'Mr.beast',
+      email: 'mrbeast@gmail.com',
+    },
+  ]
   return (
     <div className="container">
       <Header />
-      {/* <AddContact />  */}
+      <AddContact contacts={contacts} />
       {/* <ContactList /> */}
-      <ContactCard />
+      {/* <ContactCard /> */}
     </div>
   )
 }
--- src/Components/AddContact.jsx
+++ src/Components/AddContact.jsx
@@ -1,36 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { ReactComponent as ContactUser } from '../Design-images/Add-contact/contact-user.svg'
 import { ReactComponent as DeleteUser } from '../Design-images/Add-contact/delete.svg'
 import { ReactComponent as UpdateUser } from '../Design-images/Add-contact/update.svg'
 
-class AddContact extends Component {
-  render() {
+function AddContact(props) {
+  // Render function to hold data from props
+  const renderContactList = props.contacts.map((contact) => {
     return (
-      <div className="add-contact">
-        <h1 className="add-contact-title">Add Contact</h1>
-        <form className="add-contact-form">
-          <div className="input-field">
-            <label>Name</label>
-            <input type="text" placeholder="Name" required />
-          </div>
-
-          <div className="input-field">
-            <label>Email</label>
-            <input type="email" placeholder="Email" required />
-          </div>
-
-          <div className="add-button">
-            <input type="submit" value="Add" />
-          </div>
-        </form>
+      <>
         <div className="list-of-contacts">
           <div className="contact-details">
             <div className="contact-img">
               <ContactUser />
             </div>
             <div className="contact-info">
-              <span className="contact-name">Sam smith</span>
-              <span className="contact-gmail">SamSmith@gmail.com</span>
+              <span className="contact-name">{contact.name}</span>
+              <span className="contact-gmail">{contact.email}</span>
             </div>
           </div>
 
@@ -43,9 +28,30 @@ class AddContact extends Component {
             </div>
           </div>
         </div>
-      </div>
+      </>
     )
-  }
+  })
+  return (
+    <div className="add-contact">
+      <h1 className="add-contact-title">Add Contact</h1>
+      <form className="add-contact-form">
+        <div className="input-field">
+          <label>Name</label>
+          <input type="text" placeholder="Name" required />
+        </div>
+
+        <div className="input-field">
+          <label>Email</label>
+          <input type="email" placeholder="Email" required />
+        </div>
+
+        <div className="add-button">
+          <input type="submit" value="Add" />
+        </div>
+      </form>
+      <div className="all-contacts">{renderContactList}</div>
+    </div>
+  )
 }
 
 export default AddContact
